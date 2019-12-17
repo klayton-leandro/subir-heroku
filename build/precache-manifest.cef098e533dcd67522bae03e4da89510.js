8ef62ae810e029e460e43edca59b576cdf1f96d7
Klayton leandro <klayton_leandro54@hotmail.com>
2019-12-17 11:50:47 -0300

    esfera pontos

---
+++ build/precache-manifest.cef098e533dcd67522bae03e4da89510.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab7a00dec046ea3c151aa143d0d9524d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12e02af6c7c62a13571d",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "12e02af6c7c62a13571d",
+    "url": "/static/js/2.1bb7ffeb.chunk.js"
+  },
+  {
+    "revision": "2c8a4f26ee62300f97bb",
+    "url": "/static/js/main.c2b5c150.chunk.js"
+  },
+  {
+    "revision": "2889e374f4cc35c4fd37",
+    "url": "/static/js/runtime-main.848ea0d9.js"
+  },
+  {
+    "revision": "8a9cd160e712daf21931946ab419aeeb",
+    "url": "/static/media/cart.8a9cd160.svg"
+  },
+  {
+    "revision": "182482b9c8213de2b855e759cd8dd1f2",
+    "url": "/static/media/logo.182482b9.svg"
+  }
+]);
